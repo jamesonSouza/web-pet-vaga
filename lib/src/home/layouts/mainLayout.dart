9bb3fd8b7d085bd57a955312963777def552500a
Jameson Souza <jameson@callcenter.local>
2019-11-01 16:02:09 -0300

    Edicao de layout homepage

--- lib/src/home/layouts/mainLayout.dart
+++ lib/src/home/layouts/mainLayout.dart
@@ -15,7 +15,53 @@ class _HomePageState extends State<HomePage> {
         titleSpacing: 50.0,
         leading: Text('Vagas Pets',style: TextStyle(fontSize: 20),),
       ),
-      
+      body: Stack(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Column(
+
+            children: <Widget>[
+              Container(width: MediaQuery.of(context).size.width/4.0,
+              height: MediaQuery.of(context).size.height/1.1,
+              color: Colors.grey,
+              )
+            ],
+          ),
+            Container(
+              padding: EdgeInsets.all(50),
+              width:  MediaQuery.of(context).size.width/1.5,
+              child: ListView(
+              children: <Widget>[
+                Container(
+                  color: Colors.yellowAccent,
+                  width: 50,
+                  height: 50,
+                ),
+                Container(
+                  color: Colors.yellowAccent,
+                  width: 50,
+                  height: 50,
+                ),
+                
+                Container(
+                  color: Colors.yellowAccent,
+                  width: 50,
+                  height: 50,
+                ),
+                Container(
+                  color: Colors.yellowAccent,
+                  width: 50,
+                  height: 50,
+                )
+              ],
+            ),
+            )
+            
+            ],
+          )
+        ],
+      ),
     );
   }
 }
